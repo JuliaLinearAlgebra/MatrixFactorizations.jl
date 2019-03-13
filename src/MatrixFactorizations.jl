d2018dfdcdc51760d874945acc8222669c93dab3
Sheehan Olver <solver@mac.com>
2019-03-13 14:50:55 +0000

    implement reflector! to avoid bug in Julia v1.1

--- src/MatrixFactorizations.jl
+++ src/MatrixFactorizations.jl
@@ -8,7 +8,7 @@ import LinearAlgebra.LAPACK: liblapack, chkuplo, chktrans
 import LinearAlgebra: cholesky, cholesky!, norm, diag, eigvals!, eigvals, eigen!, eigen,
             qr, axpy!, ldiv!, mul!, lu, lu!, ldlt, ldlt!, AbstractTriangular, has_offset_axes,
             chkstride1, kron, lmul!, rmul!, factorize, StructuredMatrixStyle, logabsdet,
-            QRPackedQ, reflector!, reflectorApply!, AbstractQ, _zeros, _cut_B, _ret_size
+            QRPackedQ, AbstractQ, _zeros, _cut_B, _ret_size
 
 import Base: getindex, setindex!, *, +, -, ==, <, <=, >,
                 >=, /, ^, \, transpose, showerror, reindex, checkbounds, @propagate_inbounds
@@ -23,6 +23,58 @@ import Base: convert, size, view, unsafe_indices,
 
 
 export ql, ql!, QL                        
+
+# Elementary reflection similar to LAPACK. The reflector is not Hermitian but
+# ensures that tridiagonalization of Hermitian matrices become real. See lawn72
+@inline function reflector!(x::AbstractVector)
+    !has_offset_axes(x)
+    n = length(x)
+    n == 0 && return zero(eltype(x))
+    @inbounds begin
+        ξ1 = x[1]
+        normu = abs2(ξ1)
+        for i = 2:n
+            normu += abs2(x[i])
+        end
+        if iszero(normu)
+            return zero(ξ1/normu)
+        end
+        normu = sqrt(normu)
+        ν = copysign(normu, real(ξ1))
+        ξ1 += ν
+        x[1] = -ν
+        for i = 2:n
+            x[i] /= ξ1
+        end
+    end
+    ξ1/ν
+end
+
+# apply reflector from left
+@inline function reflectorApply!(x::AbstractVector, τ::Number, A::AbstractVecOrMat)
+    m,n = size(A,1),size(A,2)
+    if length(x) != m
+        throw(DimensionMismatch("reflector has length $(length(x)), which must match the first dimension of matrix A, $m"))
+    end
+    @inbounds begin
+        for j = 1:n
+            # dot
+            vAj = A[1, j]
+            for i = 2:m
+                vAj += x[i]'*A[i, j]
+            end
+
+            vAj = conj(τ)*vAj
+
+            # ger
+            A[1, j] -= vAj
+            for i = 2:m
+                A[i, j] -= x[i]*vAj
+            end
+        end
+    end
+    return A
+end
 
 include("ql.jl")
 
