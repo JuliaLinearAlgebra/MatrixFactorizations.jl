aca6de420edd5aeb4a134a3d1f54bf42b6b61388
Sheehan Olver <solver@mac.com>
2019-11-09 11:57:06 +0000

    Update project, depend on ArrayLayouts (#2)
    * Update project, depend on ArrayLayouts
    
    * Update travis

--- src/MatrixFactorizations.jl
+++ src/MatrixFactorizations.jl
@@ -1,5 +1,5 @@
 module MatrixFactorizations
-using Base, LinearAlgebra
+using Base, LinearAlgebra, ArrayLayouts
 import Base: axes, axes1, getproperty, iterate, tail
 import LinearAlgebra: BlasInt, BlasReal, BlasFloat, BlasComplex, axpy!,
                         copy_oftype, checksquare, adjoint, transpose, AdjOrTrans, HermOrSym
@@ -21,6 +21,7 @@ import Base: convert, size, view, unsafe_indices,
                                similar, copy, convert, promote_rule, rand,
                             IndexStyle, real, imag, Slice, pointer, unsafe_convert, copyto!
 
+import ArrayLayouts: reflector!, reflectorApply!
 
 if VERSION < v"1.2-"
     import Base: has_offset_axes
@@ -31,58 +32,6 @@ end
 
 export ql, ql!, qrunblocked, qrunblocked!, QL                        
 
-# Elementary reflection similar to LAPACK. The reflector is not Hermitian but
-# ensures that tridiagonalization of Hermitian matrices become real. See lawn72
-@inline function reflector!(x::AbstractVector)
-    require_one_based_indexing(x)
-    n = length(x)
-    n == 0 && return zero(eltype(x))
-    @inbounds begin
-        ξ1 = x[1]
-        normu = abs2(ξ1)
-        for i = 2:n
-            normu += abs2(x[i])
-        end
-        if iszero(normu)
-            return zero(ξ1/normu)
-        end
-        normu = sqrt(normu)
-        ν = copysign(normu, real(ξ1))
-        ξ1 += ν
-        x[1] = -ν
-        for i = 2:n
-            x[i] /= ξ1
-        end
-    end
-    ξ1/ν
-end
-
-# apply reflector from left
-@inline function reflectorApply!(x::AbstractVector, τ::Number, A::AbstractVecOrMat)
-    m,n = size(A,1),size(A,2)
-    if length(x) != m
-        throw(DimensionMismatch("reflector has length $(length(x)), which must match the first dimension of matrix A, $m"))
-    end
-    m == 0 && return A
-    @inbounds begin
-        for j = 1:n
-            # dot
-            vAj = A[1, j]
-            for i = 2:m
-                vAj += x[i]'*A[i, j]
-            end
-
-            vAj = conj(τ)*vAj
-
-            # ger
-            A[1, j] -= vAj
-            for i = 2:m
-                A[i, j] -= x[i]*vAj
-            end
-        end
-    end
-    return A
-end
 
 include("qr.jl")
 include("ql.jl")
