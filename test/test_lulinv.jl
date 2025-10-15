a1d39dedabfeb4662d3fbc9bcd521b832adb65f0
MikaelSlevinsky <richard.slevinsky@umanitoba.ca>
2025-10-15 11:39:01 -0500

    increase tests

--- test/test_lulinv.jl
+++ test/test_lulinv.jl
@@ -69,7 +69,12 @@ using LinearAlgebra, MatrixFactorizations, Random, Test
         L, U = lulinv(A)
         @test A ≈ L*U/L
         @test A*L ≈ L*U
+    end
+    @testset "size, det, logdet, logabsdet" begin
+        A = [4.0 3; 6 3]
         F = lulinv(A)
+        @test size(A) == size(F)
+        @test size(F, 1) == size(F, 2)
         @test F\A ≈ I
         @test A/F ≈ I
         @test det(A) ≈ det(F)
@@ -78,5 +83,32 @@ using LinearAlgebra, MatrixFactorizations, Random, Test
         lad1, sd1 = logabsdet(F)
         @test lad ≈ lad1
         @test sd ≈ sd1
+    end
+    @testset "REPL printing" begin
+            bf = IOBuffer()
+            show(bf, "text/plain", lulinv([2 1; 1 2]))
+            seekstart(bf)
+            @test String(take!(bf)) ==
+"""
+LULinv{Float64, Matrix{Float64}}
+L factor:
+2×2 Matrix{Float64}:
+  1.0  0.0
+ -1.0  1.0
+U factor:
+2×2 Matrix{Float64}:
+ 1.0  1.0
+ 0.0  3.0"""
+    end
+    @testset "propertynames" begin
+        names = sort!(collect(string.(Base.propertynames(lulinv([2 1; 1 2])))))
+        @test names == ["L", "U"]
+        allnames = sort!(collect(string.(Base.propertynames(lulinv([2 1; 1 2]), true))))
+        @test allnames == ["L", "U", "factors"]
+    end
+    @testset "A::Matrix{Int64}, λ::Vector{Rational{Int64}}" begin
+        A = [-150 334 778; -89 195 464; 5 -10 -27]
+        F = lulinv(A, [17, -2, 3//1])
+        @test A * F.L == F.L * F.U
     end
 end
